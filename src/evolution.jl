95229155122310234cbe6890650419f780df55e2
MathisClautrier <mclautrier98@gmail.com>
2020-11-27 11:39:43 +0100

    Choix de la représentation d'évolution, seul le dictionnaire des rangs et des distances sont spécifiés car les autres variable utiles pour NSGA2 ne le sont que pour une unique fonction, l'id sélectionné pour l'instant est l'entier retourné par objectid()

--- src/evolution.jl
+++ src/evolution.jl
@@ -1,1 +1,22 @@
-using NSGAII
+export NSGA2Evolution
+
+import Cambrian.populate, Cambrian.evaluate,  Cambrian.selection, Cambrian.generation
+
+mutable struct NSGA2Evolution{T} <: Cambrian.AbstractEvolution
+    config::NamedTuple
+    logger::CambrianLogger
+    population::Array{T}
+    fitness::Function
+    rank::Dict{UInt64,Int64}
+    distance::Dict{Uint64,Float64}
+    gen::Int
+end
+
+function NSGA2Evolution(cfg::NamedTuple, fitness::Function;
+                      logfile=string("logs/", cfg.id, ".csv"))
+    logger = CambrianLogger(logfile)
+    population = Cambrian.initialize(eval(Meta.parse(cfg.ind_type)), cfg)
+    rank=Dict{UInt64,Int64}()
+    distance=Dict{UInt64,Float64}()
+    NSGA2Evolution(cfg, logger, population, fitness,rank,distance, 0)
+end
