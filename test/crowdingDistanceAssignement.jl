dd8dc7008148139dfb284ba001051c694b30f33f
MathisClautrier <mclautrier98@gmail.com>
2020-12-02 20:19:43 +0100

    Changing tests syntax

--- test/crowdingDistanceAssignement.jl
+++ test/crowdingDistanceAssignement.jl
@@ -9,12 +9,12 @@ include("./cornercases.jl")
 @testset "crowdingDistanceAssignement" begin
     fitness1D=subPop1D()
     cfg=Cambrian.get_config("./test.yaml";n_population= 3,d_fitness=1)
-    D=Dict{UInt64,Array{Any}}()
-    fitness(x::FloatIndividual)=D[objectid(x)]
+    fitness(x::FloatIndividual)=0
     dim1=NSGA2Evolution(cfg,fitness)
     oldPop1D=copy(dim1.population)
-    D=Dict(objectid(dim1.population[i])=>fitness1D[i] for i in 1:dim1.config.n_population)
-    step!(dim1)
+    for i in 1:3
+        copyto!(dim1.population[i].fitness,fitness1D[i])
+    end
     crowdingDistanceAssignement!(dim1,dim1.population)
     @test length(oldPop1D)==length(dim1.population)
     for x in oldPop1D
@@ -23,12 +23,14 @@ include("./cornercases.jl")
     for x in dim1.population
         @test dim1.distance[objectid(x)]==0
     end
+
     fitness3D=subPop3D()
     cfg=Cambrian.get_config("./test.yaml";n_population= 5,d_fitness=3)
     dim3=NSGA2Evolution(cfg,fitness)
     oldPop3D=copy(dim3.population)
-    D=Dict(objectid(dim3.population[i])=>fitness3D[i] for i in 1:dim3.config.n_population)
-    step!(dim3)
+    for i in 1:5
+        copyto!(dim3.population[i].fitness,fitness3D[i])
+    end
     crowdingDistanceAssignement!(dim3,dim3.population)
     @test length(oldPop3D)==length(dim3.population)
     for x in oldPop3D
@@ -42,11 +44,13 @@ include("./cornercases.jl")
     for x in min3D
         @test dim3.distance[objectid(x)]==Inf
     end
+
     fitnessEQ=subPopEqual3D()
     cfg=Cambrian.get_config("./test.yaml";n_population= 8,d_fitness=3)
     equal=NSGA2Evolution(cfg,fitness)
-    D=Dict(objectid(equal.population[i])=>fitnessEQ[i] for i in 1:equal.config.n_population)
-    step!(equal)
+    for i in 1:8
+        copyto!(equal.population[i].fitness,fitnessEQ[i])
+    end
     crowdingDistanceAssignement!(equal,equal.population)
     for x in equal.population
         @test equal.distance[objectid(x)]==0
--- test/fastNonDominatedSort.jl
+++ test/fastNonDominatedSort.jl
@@ -5,12 +5,12 @@ include("./cornercases.jl")
 
 @testset "Domination" begin
     L=[[2,1,1],[1,2,2],[1,0,0]]
-    D=Dict{UInt64,Array{Any}}()
-    fitness(x::FloatIndividual)=D[objectid(x)]
+    fitness(x::FloatIndividual)=0
     cfg=Cambrian.get_config("./test.yaml";n_population= 3,d_fitness=3)
     e=NSGA2Evolution(cfg,fitness)
-    D=Dict(objectid(e.population[i])=>L[i] for i in 1:e.config.n_population)
-    step!(e)
+    for i in 1:3
+        copyto!(e.population[i].fitness,L[i])
+    end
     @test dominates(e,e.population[1],e.population[2])==false
     @test dominates(e,e.population[2],e.population[1])==false
     @test dominates(e,e.population[1],e.population[3])==true
@@ -20,13 +20,13 @@ end
 @testset "fastNonDominatedSort" begin
     fitness1,ranks1=pop1D()
     fitness2,ranks2=pop3D()
-    D=Dict{UInt64,Array{Any}}()
-    fitness(x::FloatIndividual)=D[objectid(x)]
+    fitness(x::FloatIndividual)=0
     cfg=Cambrian.get_config("./test.yaml";n_population= 20,d_fitness=1)
     e=NSGA2Evolution(cfg,fitness)
-    D=Dict(objectid(e.population[i])=>fitness1[i] for i in 1:e.config.n_population)
+    for i in 1:20
+        copyto!(e.population[i].fitness,fitness1[i])
+    end
     oldPop1D=copy(e.population)
-    step!(e)
     fastNonDominatedSort!(e)
     @test length(e.population)==length(oldPop1D)
     for x in oldPop1D
@@ -35,11 +35,13 @@ end
     for i in 1:e.config.n_population
         @test ranks1[i]==e.rank[objectid(e.population[i])]
     end
+
     cfg=Cambrian.get_config("./test.yaml";n_population= 16,d_fitness=3)
     e=NSGA2Evolution(cfg,fitness)
-    D=Dict(objectid(e.population[i])=>fitness2[i] for i in 1:e.config.n_population)
+    for i in 1:16
+        copyto!(e.population[i].fitness,fitness2[i])
+    end
     oldPop3D=copy(e.population)
-    step!(e)
     fastNonDominatedSort!(e)
     @test length(e.population)==length(oldPop3D)
     for x in oldPop3D
